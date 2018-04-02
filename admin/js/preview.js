8686d5c21795192a123078460615a055fed3935f
Bert Bruggeman <bert.bruggeman@gmail.com>
2018-04-03 00:02:21 +0200

    cloned base-jekyll-prototype

--- admin/js/preview.js
+++ admin/js/preview.js
@@ -19,8 +19,9 @@ var PagePreview = createClass({
 
     // figures
     if (images != null) {
+        var figureClass = layout == "card" && images.size == 1 ? 'wide' : 'no';
         figures = images.map(function(item, index) {
-            return h('figure', {},
+            return h('figure', {className: figureClass},
                 h('picture', {className: "preview-img", style: {'background-image': 'url("' + item.get('url') + '")'}}, "")
             );
         });
